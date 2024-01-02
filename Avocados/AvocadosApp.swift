3aa8dc471a2272f82629ae4636a5cae6da72edf9
Jonas <88572943+Johnaoe@users.noreply.github.com>
2024-01-02 22:36:26 +0200

    AppIcons

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Jonas Narbutas on 2024-01-02.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
