7c2d756ff9bf1012adeac886346fffa1524e8978
Jonas <88572943+Johnaoe@users.noreply.github.com>
2024-01-06 18:20:10 +0200

    HeaderView

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("tabicon-book")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Content")
+        ScrollView(.vertical, showsIndicators: false){
+            VStack(alignment: .center, spacing: 20) {
+                //MARK: - HEADER
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack (alignment: .top, spacing: 20){
+                        HeaderView()
+                    }
+                    
+                }
+                
+                //MARK: - FOOTER
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    Text("Everything you wanted to know about avocados")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
