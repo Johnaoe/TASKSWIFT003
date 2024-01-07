3c871ccf39c3820c2f2c317399841f6916f30a30
Jonas <88572943+Johnaoe@users.noreply.github.com>
2024-01-07 18:32:06 +0200

    Columns

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -19,14 +19,18 @@ struct ContentView: View {
                     }
                     
                 }
+                //MARK: - DISHES
+                Text("Avocado Dishes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                DishesView()
+                    .frame(maxWidth: 640)
                 
                 //MARK: - FOOTER
                 VStack(alignment: .center, spacing: 20) {
                     Text("All About Avocados")
-                        .font(.system(.title, design: .serif))
+                        .modifier(TitleModifier())
                         .fontWeight(.bold)
-                        .foregroundColor(Color("ColorGreenAdaptive"))
-                        .padding(8)
                     Text("Everything you wanted to know about avocados")
                         .font(.system(.body, design: .serif))
                         .multilineTextAlignment(.center)
@@ -39,6 +43,15 @@ struct ContentView: View {
         }
         .edgesIgnoringSafeArea(.all)
         .padding(0)
+    }
+}
+
+struct TitleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.title, design: .serif))
+            .foregroundColor(Color("ColorGreenAdaptive"))
+            .padding(8)
     }
 }
 
